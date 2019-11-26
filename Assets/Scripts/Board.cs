7c57044761dec1224dca31a4aef690374e910e61
Arun Kurian <arunkurian.dev@gmail.com>
2019-11-26 22:16:26 +0530

    Adds ChessPiece and Board code

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public ChessPiece[,] matrix;
+
+    Board()
+    {
+        matrix = new ChessPiece[8, 8]
+        {
+            { new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece() },
+            { new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece() },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { null, null, null, null, null, null, null, null },
+            { new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece() },
+            { new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece(), new ChessPiece() }
+        };
+    }
+}
---
+++ Assets/Scripts/ChessPiece.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vector2;
+
+public abstract class ChessPiece : MonoBehaviour
+{
+    private vector2<int> pos;
+    public List<vector2<int>> allpossiblemoves();
+
+    ChessPiece()
+    {
+
+    }
+
+
+}
